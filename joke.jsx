bb97d898919214c435d81519a0336b10908f886a
MukiriJoel <50231556+MukiriJoel@users.noreply.github.com>
2020-12-16 01:56:16 +0300

    Create joke.jsx

---
+++ joke.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from "react";
+import "./style.css";
+class Categories extends Component{
+  constructor(){
+    super();
+    this.state={
+      data:false
+     // jokes:[],
+     // errorMsg:''
+    }
+  }
+  ComponentDidMount(){
+    /*axios.get('https://api.chucknorris.io/jokes/categories')
+      .then(response => {
+        console.log(response)
+        this.setState({jokes: response.data})
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({errorMsg:'Error retrieving data'})
+      })*/
+      let url = "https://api.chucknorris.io/jokes/categories";
+      fetch(url,{
+        method:'GET',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json',
+        }
+      }).then((result)=>{
+        result.json().then((resp)=>{
+          
+          this.setState({data:resp})
+        })
+      })
+  }
+  render(){
+    const data=this.state.data;
+    console.warn(data);
+    return(
+      <div>
+      List of Categories
+      {
+        data ?
+        <div>
+        :<p>Name: {data.value}</p>
+        </div>
+        :<p>Please Wait..</p>
+      }
+      
+      
+      </div>
+    )
+  }
+} 
+export default Categories
